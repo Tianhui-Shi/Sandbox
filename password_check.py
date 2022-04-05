efdd129c46d4ded7c07426ce6a47ddad26e7f072
tshi9 <tshi@upei.ca>
2022-04-05 21:06:15 +0800

    password with functions

--- password_check.py
+++ password_check.py
@@ -1,68 +1,22 @@
-"""
-CP1404/CP5632 - Practical
-Password checker "skeleton" code to help you get started
-"""
+"""Module docstring"""
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = True
-SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
+# imports
+# CONSTANTS
 
 def main():
-    """Program to get and check a user's password."""
-    print("Please enter a valid password")
-    print("Your password must be between {} and {} characters, and contain:".format(MIN_LENGTH,MAX_LENGTH))
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: {}".format(SPECIAL_CHARACTERS))
-    password = input("> ")
-    while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("> ")
-    print("Your {}-character password is valid: {}".format(len(password),
-                                                           password))
+    MIN_LENGTH = int(input("PLease input min digits of password: "))
+    password = get_password(MIN_LENGTH)
+    asterisks(password)
 
+def get_password(MIN_LENGTH):
+    password = input("Please input password with {} digits".format(MIN_LENGTH))
+    while len(password) < MIN_LENGTH:
+        print("Too short \n")
+        password = input("Input your password at least {} digits".format(MIN_LENGTH))
+    return password
 
-def is_valid_password(password):
-    """Determine if the provided password is valid."""
-    if len(password)<MIN_LENGTH or len(password)>MAX_LENGTH:
-        return False
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
-    for char in password:
-        # islower checker
-        if char.islower():
-            count_lower += 1
-
-        # isupper checker
-        elif char.isupper():
-            count_upper += 1
-
-        #special checker
-        elif char in SPECIAL_CHARACTERS:
-            count_special += 1
-
-        # isdigit checker
-        elif char.isdigit():
-            count_digit += 1
-
-    # each of it must have one
-    # the sum of is >= 3
-    if count_upper==0 or count_digit==0 or count_lower==0 :
-        return False
-
-    # and return False if it's zero
-    if SPECIAL_CHARS_REQUIRED:
-        if count_special == 0:
-            return False
-
-    # if we get here (without returning False), then the password must be valid
-    return True
-
+def asterisks(password):
+    print('*' * len(password))
 
 main()
